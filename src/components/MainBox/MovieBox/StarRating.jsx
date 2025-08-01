25c9215d9514550d120b7faef2750de5f7760a9a
Shahbaaz Athhar <59239856+Shahbaaz92@users.noreply.github.com>
2025-08-01 05:35:48 +0530

    Update StarRating.jsx

--- src/components/MainBox/MovieBox/StarRating.jsx
+++ src/components/MainBox/MovieBox/StarRating.jsx
@@ -1,9 +1,9 @@
 function StarRating() {
   return (
-    <div style={}>
+    <div>
       <div>
-        {Array.from({length:5},(_,i)=>(
-          <span>S{i+1}</span>
+        {Array.from({ length: 5 }, (_, i) => (
+          <span>S{i + 1}</span>
         ))}
       </div>
       <p>10</p>
